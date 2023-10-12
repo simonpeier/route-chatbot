6af0302fbf4a678bf0dbcbe830e8984d16ac826a
Roger Marty <rogermarty742@gmail.com>
2023-10-12 14:16:07 +0200

    prepared directions API request structure and response handling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
 
+import requests
 from google.cloud import dialogflow
 
 # path to the key-file
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "aifo-miniproject-401806-0b78e72e0f2f.json"
+api_key = "AIzaSyCEnDQGxcTkbQVazIXFO-E081PYAvciANY"
+api_url = 'https://maps.googleapis.com/maps/api/directions/json?'
 
 
 # example from  https://cloud.google.com/dialogflow/es/docs/quick/api#detect-intent-text-python
@@ -44,7 +47,26 @@ def detect_intent_demo(project_id, session_id, texts, language_code):
         print(f"Destination: {response.query_result.parameters['destination']['city']}")
         print(f"Travelmode: {response.query_result.parameters['travelmode']}")
 
+        params = {
+            'origin': response.query_result.parameters['origin']['city'],
+            'destination': response.query_result.parameters['destination']['city'],
+            'travel_mode': response.query_result.parameters['travelmode'],
+            'key': api_key
+        }
+
+        response = requests.get(api_url, params=params)
+        print(response.text)
+
+        if response.status_code == 200:
+            data = response.json()
+            distance = data['routes'][0]['legs'][0]['distance']['text']
+            duration = data['routes'][0]['legs'][0]['duration']['text']
+            print(f"Distance: {distance}")
+            print(f"Duration: {duration}")
+        else:
+            print(f"Request failed with status code {response.status_code}")
+
 
 if __name__ == "__main__":
     # change the values accordingly
-    detect_intent_demo('aifo-miniproject-401806', 'no-session-ID', ['I want to go from Genf to Tokyo by car'], 'en')
+    detect_intent_demo('aifo-miniproject-401806', 'no-session-ID', ['I want to go from Winterthur to Genf by car'], 'en')
