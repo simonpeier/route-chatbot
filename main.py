ec812333c8b16deac3448b2a9ec2dd3e14f80722
Simon Peier <simon.w.peier@gmail.com>
2023-10-24 16:40:25 +0200

    Clean up code and improve readability

--- main.py
+++ main.py
@@ -19,50 +19,32 @@ def detect_intent(text):
     language_code = 'en'
 
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
-    # print("Session path: {}\n".format(session))
 
-    while True:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
 
-        query_input = dialogflow.QueryInput(text=text_input)
+    print(f"INTENT: {response.query_result.intent.display_name}")
+    print(f"PARAMETERS: {response.query_result.fulfillment_text}\n")
 
-        response = session_client.detect_intent(
-            request={"session": session, "query_input": query_input}
-        )
-
-        # if response.query_result.intent.display_name == "Default Fallback Intent":
-        #     text = input(response.query_result.fulfillment_text + "\n")
-        #     print("TEXT: " + text)
-        if response.query_result.fulfillment_text != "completed":
-            print(f"INTENT:\n {response.query_result.intent.display_name}")
-            print(f"PARAMETERS:\n {response.query_result.fulfillment_text}")
-            return [response.query_result.fulfillment_text]
-            # text = input(response.query_result.fulfillment_text + "\n")
-        else:
-            # print(response.query_result.fulfillment_text + "\n")
-            # print("=" * 20)
-            # print("Query text: {}".format(response.query_result.query_text))
-            # print(
-            #     "Detected intent: {} (confidence: {})\n".format(
-            #         response.query_result.intent.display_name,
-            #         response.query_result.intent_detection_confidence
-            #     )
-            # )
-            question_text = "Do you need something else?"
-            if response.query_result.intent.display_name == "request.route":
-                origin = response.query_result.parameters['origin']['city']
-                destination = response.query_result.parameters['destination']['city']
-                travelmode = response.query_result.parameters['travelmode']
-                print_dialogflow_query_parameters(destination, origin, response, travelmode)
-                return [fetch_route(destination, origin, travelmode), question_text]
-            elif response.query_result.intent.display_name == "find.place":
-                location = response.query_result.parameters['location']['city']
-                type = response.query_result.parameters['placetype']
-                print(f"Location: {location}")
-                print(f"Type: {type}\n")
-                return [fetch_places(location, type), question_text]
+    if response.query_result.fulfillment_text != "completed":
+        return [response.query_result.fulfillment_text]
+    else:
+        question_text = "Do you need something else?"
+        if response.query_result.intent.display_name == "request.route":
+            origin = response.query_result.parameters['origin']['city']
+            destination = response.query_result.parameters['destination']['city']
+            travelmode = response.query_result.parameters['travelmode']
+            print_route_parameters(destination, origin, travelmode)
+            return [fetch_route(destination, origin, travelmode), question_text]
+        elif response.query_result.intent.display_name == "find.place":
+            location = response.query_result.parameters['location']['city']
+            placetype = response.query_result.parameters['placetype']
+            print_place_parameters(location, placetype)
+            return [fetch_places(location, placetype), question_text]
 
 
 def fetch_route(destination, origin, travelmode):
@@ -73,7 +55,6 @@ def fetch_route(destination, origin, travelmode):
         'key': api_key
     }
     response = requests.get(directions_url, params=params)
-    # print(f"Directions api url: {response.url}\n")
 
     if response.status_code == 200:
         routes = response.json()['routes']
@@ -87,19 +68,18 @@ def fetch_route(destination, origin, travelmode):
         return f"Request failed with status code {response.status_code}"
 
 
-def fetch_places(location, type):
+def fetch_places(location, placetype):
     params = {
         'query': location,
-        'type': type,
+        'type': placetype,
         'key': api_key
     }
     response = requests.get(places_url, params=params)
-    # print(f"Directions api url: {response.url}\n")
 
     if response.status_code == 200:
         results = response.json()['results']
         if not results:
-            return f"Looks like there is no {type} in {location}"
+            return f"Looks like there is no {placetype} in {location}"
         else:
             if len(results) < 3:
                 name = results[0]['name']
@@ -116,11 +96,15 @@ def fetch_places(location, type):
         return f"Request failed with status code {response.status_code}"
 
 
-def print_dialogflow_query_parameters(destination, origin, response, travelmode):
-    # print("Fulfillment text: {}".format(response.query_result.fulfillment_text))
+def print_route_parameters(destination, origin, travelmode):
     print(f"Origin:  {origin}")
     print(f"Destination: {destination}")
     print(f"Travelmode: {travelmode}\n")
+
+
+def print_place_parameters(location, placetype):
+    print(f"Location: {location}")
+    print(f"Type: {placetype}\n")
 
 
 @app.route("/")
@@ -132,7 +116,6 @@ def home():
 def get_bot_response():
     user_text = request.args.get("msg")
     bot_response = detect_intent(user_text)
-
     return bot_response
 
 
